43856c76caf436082436aeb7371b61506fd52a9f
rain <463728946@qq.com>
2019-06-02 19:31:48 -0700

    添加项目文件。

---
+++ LM Direct Host/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LM_Direct_Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
